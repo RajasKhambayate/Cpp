bbb5ef62ae5aacd4fa9492f02cd8205a945c0744
Rajas <RajasKhambayate8@gmail.com>
2025-07-30 06:01:08 +0530

    This program checks weather a number is prime or composite .

---
+++ Problems_on_Numbers/Check_Prime.cpp
@@ -0,0 +1,116 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//Description: This program checks weather a number is prime or composite .                       //
+//================================================================================================//
+//Language: C++                                                                                   //
+//Compiler : GNU G++                                                                              //
+//IDE: Visual Studio code                                                                         //
+//================================================================================================//
+//Author/Coder: Rajas Khambayate                                                                  //
+//Date: 16th May 2025                                                                             //
+//Day: Friday                                                                                     //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<iostream>
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//Check_Prime() Function: This function determines whether number is prime or composite .         //
+//================================================================================================//
+//Parameters:                                                                                     //
+//1. int(iValue) : The value to determine prime or composite .                                    //
+//================================================================================================//
+//Return: boolean                                                                                 //
+//================================================================================================//
+//Local variables:                                                                                //
+//1. int(iFact) : To count the number of factors of 'iValue' .                                    //
+//2. int(iCnt) : Loop counter variable .                                                          //
+//================================================================================================//
+//Algorithm:                                                                                      //
+//1. Initialize 'iFact' to 0.                                                                     //
+//2. Loop 'iCnt' from 1 to 'iValue/2':                                                            //
+//   a. If 'iValue' is divisible by 'iCnt', increment 'iFact'.                                    //
+//3. End of loop.                                                                                 //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+bool Chk_Prime(int iValue)
+{
+    if((iValue == 0) || (iValue == 1))
+    {
+        return false;
+    }
+
+    int iFact = 0;
+
+    for(int iCnt = 1;iCnt <= iValue/2;iCnt++)
+    {
+        if((iValue % iCnt) == 0)
+        {
+            iFact++;
+        }
+    }
+
+    if(iFact == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//Starting point of application
+int main()
+{
+    int iNo = -1;
+
+    while(1)
+    {
+        while(iNo < 0)
+        {
+            cout<<endl<<"Please enter a positive integer value : ";
+            cin>>iNo;
+
+            if(iNo < 0)
+            {
+                continue;
+            }
+        }
+
+        if(iNo == 0 || iNo == 1)
+        {
+            cout<<"0 and 1 are neither prime nor composite numbers ."<<endl;
+            cout<<"===================================================================================================="<<endl;
+
+            iNo = -1;
+
+            continue;
+        }
+
+        bool bRet = Chk_Prime(iNo);
+        if(bRet == true)
+        {
+            cout<<endl<<iNo<<" : It is prime number"<<endl;
+        }
+        else
+        {
+            cout<<endl<<iNo<<" : It is composite number"<<endl;
+        }
+
+        cout<<"Would you like to try Check_Prime one more time? >Press for->YES:ANY_NUM OR NO:0 <=>Your Choice : ";
+        cin>>iNo;
+
+        if(iNo == 0)
+        {
+            cout<<endl<<"===================================================================================================="<<endl;
+            cout<<"Thank you for using Rajas's Check_Prime program ."<<endl;
+            break;
+        }
+        else
+        {
+            cout<<"===================================================================================================="<<endl;
+            iNo = -1;
+            continue;
+        }
+    }
+
+    return 0;
+}
